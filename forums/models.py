dcd52bedf223c5c9e8e4cdff03d9d892638bb9ef
domsq <Domsq@hotmail.com>
2021-11-25 15:50:15 +0000

    Add Topic class to models.py

--- forums/models.py
+++ forums/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Topic(models.Model):
+    """Schema for the Topic model"""
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
