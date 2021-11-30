13c3e4780e7aae07c6ffdd2afde449227ce38046
domsq <Domsq@hotmail.com>
2021-11-30 17:00:08 +0000

    Add Post model to admin view

--- forums/admin.py
+++ forums/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Topic
+from .models import Topic, Post
 
 
 @admin.register(Topic)
@@ -7,3 +7,11 @@ class TopicAdmin(admin.ModelAdmin):
     """Admin view for Topics"""
     list_display = ('name', 'description')
     search_fields = ('name', 'description')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Admin view for Posts"""
+    list_display = ('title', 'slug', 'created', 'creator')
+    search_fields = ('title', 'body', 'creator')
+    list_filter = ('creator', 'created')
