d431d9a34b538d5042499ee6af4b7ac3e2caebc4
domsq <Domsq@hotmail.com>
2021-12-10 17:01:47 +0000

    Update views.py to render list of topics

--- forums/views.py
+++ forums/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Topic
 
 
-def home(request):
-    return render(request, 'base.html')
+class TopicList(generic.ListView):
+    """ View to render the topics list """
+    model = Topic
+    queryset = Topic.objects.order_by('name')
+    template_name = 'index.html'
+    paginate_by = 6
+
