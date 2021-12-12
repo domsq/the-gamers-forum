c42c81e9b4ef8cac4328efb91c4e87a546597a46
domsq <Domsq@hotmail.com>
2021-12-12 18:56:32 +0000

    Update TopicList view to include recent posts

--- forums/views.py
+++ forums/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Topic
+from .models import Post, Topic
 
 
 class TopicList(generic.ListView):
     """ View to render the topics list """
     model = Topic
-    queryset = Topic.objects.order_by('name')
-    template_name = 'index.html'
-    paginate_by = 6
+    template_name = 'index.html'    
 
+    def get_context_data(self, **kwargs):
+        context = super(TopicList, self).get_context_data(**kwargs)
+        context['posts'] = Post.objects.order_by('-created')
+        return context
