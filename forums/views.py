26bbfb1c32600afd6cc945c13d198511731b26d5
domsq <Domsq@hotmail.com>
2021-12-14 15:22:28 +0000

    Add view for topic detail to views.py

--- forums/views.py
+++ forums/views.py
@@ -1,14 +1,31 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
 from .models import Post, Topic
 
 
 class TopicList(generic.ListView):
     """ View to render the topics list """
     model = Topic
-    template_name = 'index.html'    
+    template_name = 'index.html'
 
     def get_context_data(self, **kwargs):
         context = super(TopicList, self).get_context_data(**kwargs)
         context['posts'] = Post.objects.order_by('-created')
         return context
+
+
+class TopicDetail(View):
+    """View to render detail for a chosen topic"""
+    def get(self, request, name, *args, **kwargs):
+        queryset = Topic.objects
+        topic = get_object_or_404(queryset, name=name)
+        posts = topic.topic_posts.order_by('-created')
+
+        return render(
+            request,
+            'topic_detail.html',
+            {
+                "topic": topic,
+                "posts": posts
+            },
+        )
