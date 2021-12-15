b2bb60c991447bf027be8da08a605c9b1d0fe2d4
domsq <Domsq@hotmail.com>
2021-12-15 17:38:55 +0000

    Update urls.py for post detail view

--- forums/urls.py
+++ forums/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.TopicList.as_view(), name='homepage'),
     path('topic/<str:name>/', views.TopicDetail.as_view(), name='topic_detail'),
+    path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
