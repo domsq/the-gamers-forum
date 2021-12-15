9fad8d3477f579f994a2d9969802ee0b92b150d7
domsq <Domsq@hotmail.com>
2021-12-15 17:40:01 +0000

    Install Crispy Forms, configure settings and requirements

--- gamersforum/settings.py
+++ gamersforum/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
+    'crispy_forms',
     'forums',
 ]
 
@@ -60,6 +61,8 @@ SITE_ID = 1
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
