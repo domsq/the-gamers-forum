923fbd5a0654f86eab9f69f9d07c37dc6e30ee5b
domsq <Domsq@hotmail.com>
2021-12-17 13:00:35 +0000

    Update urls.py for new postadd view

--- forums/urls.py
+++ forums/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TopicList.as_view(), name='homepage'),
     path('topic/<str:name>/', views.TopicDetail.as_view(), name='topic_detail'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('add-post/', views.PostAdd.as_view(), name='add_post'),
 ]
