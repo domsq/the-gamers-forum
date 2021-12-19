f7a108617b9c0437a0ab427b2315f56b77fc296f
domsq <Domsq@hotmail.com>
2021-12-19 15:15:15 +0000

    Update urls.py for new postedit view

--- forums/urls.py
+++ forums/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('topic/<str:name>/', views.TopicDetail.as_view(), name='topic_detail'),
     path('post/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('add-post/', views.PostAdd.as_view(), name='add_post'),
+    path('edit-post/<slug:slug>/', views.PostEdit.as_view(), name='edit_post'),
 ]
