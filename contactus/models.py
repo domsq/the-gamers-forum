b5cb53ba9d9ca9a0e12e8922933cd0675bb301b7
domsq <Domsq@hotmail.com>
2021-12-20 12:26:02 +0000

    Move the Contact model from forums to contactus

--- contactus/models.py
+++ contactus/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    """Schema for the Contact model, to be used for a contact form"""
+    fname = models.CharField(max_length=150)
+    lname = models.CharField(max_length=150)
+    email = models.EmailField()
+    body = models.TextField()
+
+    def __str__(self):
+        return f"Message {self.body} from {self.fname} {self.lname}"
--- forums/models.py
+++ forums/models.py
@@ -74,14 +74,3 @@ class Reply(models.Model):
     class Meta:
         """Sorts the posts in descending order"""
         ordering = ["-created"]
-
-
-class Contact(models.Model):
-    """Schema for the Contact model, to be used for a contact form"""
-    fname = models.CharField(max_length=150)
-    lname = models.CharField(max_length=150)
-    email = models.EmailField()
-    body = models.TextField()
-
-    def __str__(self):
-        return f"Message {self.body} from {self.fname} {self.lname}"
