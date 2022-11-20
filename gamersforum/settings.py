bae36ed75832457a07fcaaef760c17232ad7738d
Dominic Quail <Domsq@hotmail.com>
2022-11-20 22:00:25 +0000

    Prepare application for render.com

--- gamersforum/settings.py
+++ gamersforum/settings.py
@@ -38,6 +38,11 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['the-gamers-forum.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+ if RENDER_EXTERNAL_HOSTNAME:
+   ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
